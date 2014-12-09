dc4d8caa4cdc8e6a425594eef18cd510e88ff791
Liz Hubertz <liz.hubertz@gmail.com>
2014-12-09 15:09:42 -0800

    add cover photo to albums

--- app/models/album.rb
+++ app/models/album.rb
@@ -2,4 +2,8 @@ class Album < ActiveRecord::Base
   has_many :photos
 
   accepts_nested_attributes_for :photos
+
+  def cover_photo_url
+    photos.first.upload(:medium) rescue "bay-bridge.jpg"
+  end
 end
