7ded3580c8f4213049ffdfe5a988280b8373dcc2
Liz Hubertz <liz.hubertz@gmail.com>
2015-02-21 13:42:55 +0700

    process images so they don't have white space on top and bottom

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -33,7 +33,7 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   version :large do
-    process :resize_and_pad => [800, 800] 
+    process :resize_to_fit => [800, 10000]
   end
   version :medium do
     # returns a 50x50 image
