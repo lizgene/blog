dc3c692d2782e616fd54ba532aa02c11e0c28d7f
Liz Hubertz <liz.hubertz@gmail.com>
2015-03-09 15:54:11 +0700

    album parameterize change, plus a real google map for the trips page

--- app/models/album.rb
+++ app/models/album.rb
@@ -7,7 +7,7 @@ class Album < ActiveRecord::Base
   before_validation :set_slug, :on => :create
 
   def set_slug
-    self.slug = self.title.downcase.gsub(" ", "-")
+    self.slug = self.title.parameterize 
   end
 
   def to_param
