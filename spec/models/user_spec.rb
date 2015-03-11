d30786932db9c19c73893a192942ea6979ba2bf3
Liz Hubertz <liz.hubertz@gmail.com>
2015-03-11 11:54:38 +0700

    pending tests for users

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  pending "has a valid factory"
+  pending "fails validation if email is not present"
+  pending "fails validation if password is not present"
+  pending "successfully logs in with Google OAuth"
+  pending "successfully logs in without Google OAuth"
+end
