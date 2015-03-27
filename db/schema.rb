fe6d48717b936e43f0bd59746266ff8013b23676
Liz Hubertz <liz.hubertz@gmail.com>
2015-03-27 16:25:08 +0700

    add title to checkpoints

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150325140712) do
+ActiveRecord::Schema.define(version: 20150327092240) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 20150325140712) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "post_id"
+    t.string   "title"
   end
 
   create_table "delayed_jobs", force: true do |t|
