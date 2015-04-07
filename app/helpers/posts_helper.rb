b8c49412052c5790178cbacf44636c817d438c66
Liz Hubertz <liz.hubertz@gmail.com>
2015-04-07 14:57:05 +0800

    add share to twitter and facebook functionality

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,17 @@
 module PostsHelper
+  require 'uri'
+
+  def html_safe_url post
+    return URI.escape(post_url(post))
+  end
+
+  def facebook_share_url post
+    return "http://www.facebook.com/sharer.php?s=100&p[url]=#{html_safe_url(post)}"
+  end
+
+  def twitter_share_url post
+    text = URI.escape("Awesome article - worth a read!")
+
+    return "https://twitter.com/intent/tweet?text=#{text}&url=#{html_safe_url(post)}&via=lizhubertz"
+  end
 end
