1727dbfb7f662f2730ffb86023303e03daa1fd53
Liz Hubertz <liz.hubertz@gmail.com>
2015-05-12 22:16:18 +0900

    javascript map improvement

--- app/assets/javascripts/google_maps.js
+++ app/assets/javascripts/google_maps.js
@@ -10,12 +10,17 @@ function gmap_show(checkpoints) {
     var checkpoint_markers = [{ "lat": "7.6556", "lng": "99.0419" }]
   }
 
+  //used for zoom
+  var last_checkpoint = valid_checkpoints[valid_checkpoints.length - 1]
+
   handler = Gmaps.build('Google');    // map init
   handler.buildMap({ provider: {}, internal: {id: 'map'}}, function(){
     markers = handler.addMarkers(checkpoint_markers)
     handler.bounds.extendWith(markers);
-    handler.fitMapToBounds();
-    handler.getMap().setZoom(5);
+    // handler.fitMapToBounds(markers);
+
+    handler.getMap().setZoom(15);
+    handler.map.centerOn([last_checkpoint.latitude, last_checkpoint.longitude])
   });
 
 }
